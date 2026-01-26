b10a00a057963379788027123cfd8b40ee451e0f
jhin1m <ducanhfake@gmail.com>
2026-01-26 15:13:26 +0700

    feat: Update and standardize translations across multiple languages and integrate them into the authentication modal.

--- resources/js/components/AuthModal.vue
+++ resources/js/components/AuthModal.vue
@@ -1,37 +1,34 @@
 <template>
-  <div class="modal fade" id="auth-modal">
-    <div class="modal-dialog modal-dialog-centered modal-sm">
-      <div class="modal-content">
-        <transition name="fade" mode="out-in">
-          <component :modal="true" :is="component" v-if="component"></component>
-        </transition>
-      </div>
+    <div class="modal fade" id="auth-modal">
+        <div class="modal-dialog modal-dialog-centered modal-sm">
+            <div class="modal-content">
+                <transition name="fade" mode="out-in">
+                    <component :modal="true" :is="component" v-if="component"></component>
+                </transition>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
+import Login from './auth/Login';
+import Register from './auth/Register';
+import PasswordReset from './auth/PasswordReset';
+
 export default {
     name: "auth-modal",
+    components: {
+        Login,
+        Register,
+        PasswordReset
+    },
     data() {
         return {
-            component: {
-                template: '<div class="spinner-border my-5 mx-auto"></div>'
-            }
+            component: null
         };
     },
     methods: {
-        openModal(component) {
-            this.component = () => ({
-                component: import(/* webpackMode: "lazy-once", webpackChunkName: "auth.bundle" */ `./auth/${component}`),
-                loading: {
-                    template: '<div class="spinner-border my-5 mx-auto"></div>'
-                },
-                error: {
-                    template: '<div class="my-5 mx-auto">{{ $t("app.something_went_wrong") }}</div>'
-                },
-                delay: 200,
-                timeout: 5000
-            })
+        openModal(componentName) {
+            this.component = componentName;
             var modal = document.querySelector('#auth-modal');
             if (!modal.classList.contains('show')) {
                 new this.$bootstrap.Modal(modal).show();
